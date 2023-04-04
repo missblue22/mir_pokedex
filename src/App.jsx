414e82c357e06dd67b10edfea41367e825b30b15
Missblue <mireille.dert@gmail.com>
2023-04-04 14:54:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css'
+import PokemonCard from './components/PokemonCard'
+
+
+function App() {
+  return (
+    <div>
+    <PokemonCard /><p>salutmireille</p>
+    </div>
+  );
+}
+
+
+export default App;
+
+
